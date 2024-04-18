b181025631914c3778bbc6d28afdf6483523a293
devitoangelo <angelodevito4@gmail.com>
2024-04-18 18:20:36 +0200

    add file db and pass the Production

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <?php
 require_once __DIR__ . '/models/Production.php';
-
-$american_sniper = new Production('American Sniper', 'Inglese','8/10');
-
-// $american_sniper->titolo = 'American Sniper';
-// $american_sniper->lingua = 'Inglese';
-// $american_sniper->voto = '8/10';
-
-$matrix = new Production('The Matrix', 'Inglese', '7/10');
-
-// $matrix->titolo = 'The Matrix';
-// $matrix->lingua = 'Inglese';
-// $american_sniper->voto = '7/10';
-
-$avatar = new Production('Avatar', 'Inglese', '8/10');
+require_once __DIR__ .'/models/db.php';
+?>
 
 
-var_dump($american_sniper, $matrix, $avatar);
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 
 
