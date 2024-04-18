9b1d7e2e6b4ff9d05244a4e798c8c34ef3798ed4
devitoangelo <angelodevito4@gmail.com>
2024-04-18 18:16:20 +0200

    add method in function production

--- index.php
+++ index.php
@@ -9,12 +9,9 @@ $american_sniper = new Production('American Sniper', 'Inglese','8/10');
 
 $matrix = new Production('The Matrix', 'Inglese', '7/10');
 
-
 // $matrix->titolo = 'The Matrix';
 // $matrix->lingua = 'Inglese';
 // $american_sniper->voto = '7/10';
-
-
 
 $avatar = new Production('Avatar', 'Inglese', '8/10');
 
--- models/Production.php
+++ models/Production.php
@@ -2,16 +2,13 @@
 
 class Production
 {
-    public $titolo;
-    public $lingua;
-    public $voto;
+    // public $titolo;
+    // public $lingua;
+    // public $voto;
 
-
-    public function __construct($titolo, $lingua, $voto)
+    public function __construct(public string $titolo, public string $lingua, public string $voto)
     {
-        $this->titolo = $titolo;
-        $this->lingua = $lingua;
-        $this->voto = $voto;
-
+        
     }
+
 };
