282c0ba2d331019e928dca40117ce6f238078fa1
devitoangelo <angelodevito4@gmail.com>
2024-04-19 17:06:37 +0200

    add new class and dump in page

--- models/Production.php
+++ models/Production.php
@@ -8,7 +8,62 @@ class Production
 
     public function __construct(public string $titolo, public string $lingua, public string $voto)
     {
-        
     }
 
-};
+    public function getVote()
+    {
+        if ($this->voto > 6) {
+            return "Voto ok ";
+        } else {
+            return "voto sotto la media";
+        }
+    }
+
+
+    //su get serve il return su post non si mette il returni ma solo this nelle funzioni 
+}
+
+
+class Movie 
+{
+
+    public $profitti;
+    public $durata;
+
+    public function __construct($profitti, $durata,){
+
+        $this->profitti = $profitti;
+        $this->durata = $durata;
+    }
+}
+
+
+$movie = new Movie(20, '50 minuti');
+
+
+var_dump($movie);
+
+class TVSerie 
+{
+
+    public $numerostagioni;
+
+
+    public function __construct($numerostagioni){
+
+        $this->numerostagioni = $numerostagioni;
+
+
+    }
+}
+
+
+$movie = new Movie(20, '1 minuti');
+
+
+// var_dump($movie);
+
+
+$tvStagioni = new TVSerie(3,);
+
+var_dump($tvStagioni->numerostagioni);
--- models/db.php
+++ models/db.php
@@ -1,6 +1,6 @@
 <?php
 $american_sniper = new Production('American Sniper', 'Inglese', '8/10');
-$matrix = new Production('The Matrix', 'Inglese', '7/10');
+$matrix = new Production('The Matrix', 'Inglese', '4/10');
 $avatar = new Production('Avatar', 'Inglese', '8/10',);
 
 
